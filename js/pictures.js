fc09c304dca66c7368cd2cec22565430acd0a8b2
Kagero <lj.chistova@yandex.ru>
2018-11-26 20:13:15 +0300

    full task

--- js/pictures.js
+++ js/pictures.js
@@ -4,10 +4,10 @@ var Picture = {
   COUNT: 25,
   MIN_LIKES: 15,
   MAX_LIKES: 200,
-  MIN_COMMENTS: 1,
+  MIN_COMMENTS: 0,
   MAX_COMMENTS: 10,
 
-  comments: [
+  COMMENTS: [
     'Всё отлично!',
     'В целом всё неплохо. Но не всё.',
     'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -16,7 +16,7 @@ var Picture = {
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
   ],
 
-  description: [
+  DESCRIPTION: [
     'Тестим новую камеру!',
     'Затусили с друзьями на море',
     'Как же круто тут кормят',
@@ -30,6 +30,8 @@ var picturesList = document.querySelector('.pictures');
 var pictureTemplate = document.querySelector('#picture').content;
 var bigPicture = document.querySelector('.big-picture');
 var commentsList = bigPicture.querySelector('.social__comments');
+var commentCount = bigPicture.querySelector('.social__comment-count');
+var commentsLoader = bigPicture.querySelector('.comments-loader');
 
 var getRandomInRange = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
@@ -43,31 +45,29 @@ var getRandomElement = function (arr) {
 };
 
 var getComments = function (arr, min, max) {
-  var test = [];
+  var results = [];
   var quantity = getRandomInRange(min, max);
   for (var i = 0; i < quantity; i++) {
-    var index = getRandomInRange(min, arr.length);
-    test.push(arr[index]);
+    var index = getRandomInRange(min, arr.length - 1);
+    results.push(arr[index]);
   }
-  return test;
+  return results;
 };
 
 var generatePictures = function () {
   var pictures = [];
+
   for (var i = 1; i <= Picture.COUNT; i++) {
     pictures.push({
       url: 'photos/' + i + '.jpg',
       likes: getRandomInRange(Picture.MIN_LIKES, Picture.MAX_LIKES),
-      comments: getComments(Picture.comments, Picture.MIN_COMMENTS, Picture.MAX_COMMENTS),
-      description: getRandomElement(Picture.description)
+      comments: getComments(Picture.COMMENTS, Picture.MIN_COMMENTS, Picture.MAX_COMMENTS),
+      description: getRandomElement(Picture.DESCRIPTION)
     });
   }
 
   return pictures;
 };
-
-var pictures = generatePictures(); // for TEST!!!
-// console.log(pictures);
 
 var renderPicture = function (picture) {
   var pictureElement = pictureTemplate.cloneNode(true);
@@ -89,28 +89,14 @@ var createPictures = function (arr) {
   picturesList.appendChild(fragment);
 };
 
-var makeElement = function (tagName, className, text) {
+var makeElement = function (tagName, className) {
   var element = document.createElement(tagName);
   element.classList.add(className);
-  if (text) {
-    element.textContent = text;
-  }
+
   return element;
 };
 
-var bletcom = function (arr) { // это чушь!!
-  for (var j = 0; j < arr.length; j++) {
-    var x = arr[j];
-
-  }
-  return x;
-};
-
-// console.log(pictures[0].comments[0]);
-// console.log(pictures[0].comments[1]);
-// console.log(pictures[0].comments[3]);
-// console.log(bletcom(pictures[0].comments));
-var createComment = function () {
+var createComment = function (item) {
   var listItem = makeElement('li', 'social__comment');
 
   var image = makeElement('img', 'social__picture');
@@ -120,23 +106,22 @@ var createComment = function () {
   image.height = '35';
   listItem.appendChild(image);
 
-  var comText = makeElement('p', 'social__text');
-  comText.textContent = bletcom(pictures[0].comments); // как докопаться до коммента из pictures?
-  listItem.appendChild(comText);
-  // console.log(comText);
+  var commentText = makeElement('p', 'social__text');
+  commentText.textContent = item;
+  listItem.appendChild(commentText);
 
   return listItem;
 };
 
-var com = function () {
+var renderComments = function (arr) {
 
   while (commentsList.firstChild) {
     commentsList.removeChild(commentsList.firstChild);
   }
 
-  for (var i = 0; i < pictures[0].comments.length; i++) {
-    var cardItem = createComment(pictures[0].comments[i]);
-    commentsList.appendChild(cardItem);
+  for (var i = 0; i < arr.length; i++) {
+    var commentsItem = createComment(arr[i]);
+    commentsList.appendChild(commentsItem);
   }
 
 };
@@ -146,26 +131,22 @@ var renderBigPicture = function (picture) {
   bigPicture.querySelector('.big-picture__img img').src = picture.url;
   bigPicture.querySelector('.likes-count').textContent = picture.likes;
   bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
-  com();
+  renderComments(picture.comments);
   bigPicture.querySelector('.social__caption').textContent = picture.description;
 
   return bigPicture;
 };
 
-var showPictures = function () {
-  // var pictures = generatePictures();
-  createPictures(pictures);
+var pictures = generatePictures();
 
-};
-
-showPictures();
+createPictures(pictures);
 
 var showBigPicture = function () {
-  // var a = generatePictures();
+
   renderBigPicture(pictures[0]);
   bigPicture.classList.remove('hidden');
-  bigPicture.querySelector('.social__comment-count').classList.add('visually-hidden'); // временно для теста
-  bigPicture.querySelector('.comments-loader').classList.add('visually-hidden'); // временно для теста
+  commentCount.classList.add('visually-hidden');
+  commentsLoader.classList.add('visually-hidden');
 };
 
 showBigPicture();
